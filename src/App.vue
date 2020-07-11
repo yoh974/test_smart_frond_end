40ede00d40f36ca19c934edcbb754acc14066c4d
lionel <sornom.lionel@gmail.com>
2020-07-11 22:55:45 +0200

    add axios and test api responses

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,105 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+
+        <div>
+            <div>Nombre total d'oeuvres : {{nb_total_items}}</div>
+            <Listing
+                    :artworks="artworks"
+            />
+            <ul class="pagination">
+                <li class="page-item"><span class="page-link" >Previous</span></li>
+                <li class="page-item"><span class="page-link" >1</span></li>
+                <li class="page-item"><span class="page-link" v-on:click="getPageResult(2)">2</span></li>
+                <li class="page-item"><span class="page-link">3</span></li>
+                <li class="page-item"><span class="page-link">Next</span></li>
+            </ul>
+
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Listing from './components/Listing.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import axios from "axios"
+
+
+    export default {
+
+        name: 'App',
+        components: {
+            Listing
+        },
+        data() {
+            return {
+                artworks: null,
+                nb_total_items: null
+            }
+        },
+        methods: {
+            getPageResult: function (page) {
+                axios({
+                    method: 'get',
+                    url: `http://localhost:8000/api/libraries?page=${page}`,
+                    responseType: 'application/json'
+                })
+                    .then(response => {
+                        this.artworks = response["data"]["hydra:member"]
+
+                    })
+            }
+        },
+        mounted() {
+            if (this.artworks === null) {
+                axios({
+                    method: 'get',
+                    url: 'http://localhost:8000/api/libraries',
+                    responseType: 'application/json',
+                })
+                    .then(response => {
+                        this.artworks = response["data"]["hydra:member"]
+                        this.nb_total_items = response["data"]["hydra:totalItems"]
+                    })
+            }
+
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+    ul.pagination {
+        display: inline-block;
+        padding: 0;
+        margin: 0;
+    }
+
+    ul.pagination li {
+        display: inline;
+    }
+
+    ul.pagination li span {
+        color: black;
+        float: left;
+        padding: 8px 16px;
+        text-decoration: none;
+    }
+
+    ul.pagination li span.active {
+        background-color: #4CAF50;
+        color: white;
+    }
+
+    ul.pagination li span:hover:not(.active) {
+        background-color: #ddd;
+        cursor: pointer;
+    }
+
 </style>
---
+++ src/components/Listing.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <table id="artworks-table">
+            <tr>
+                <th>Titre</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Editeur</th>
+                <th>Format livre</th>
+                <th>Type</th>
+                <th>Section</th>
+                <th>Etagère</th>
+                <th>Rangée</th>
+            </tr>
+            <tr
+                    :artwork="artwork"
+                    :key="artwork.id"
+                    v-for="artwork in artworks"
+            >
+                <td>{{artwork.title}}</td>
+                <td>{{artwork.name}}</td>
+                <td>{{artwork.firstname}}</td>
+                <td>{{artwork.editor}}</td>
+                <td>{{artwork.bookFormat}}</td>
+                <td>{{artwork.type}}</td>
+                <td>{{artwork.section}}</td>
+                <td>{{artwork.shelf}}</td>
+                <td>{{artwork.row}}</td>
+            </tr>
+
+        </table>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Listing",
+        props:['artworks'],
+    }
+</script>
+
+<style scoped>
+    #artworks-table {
+        font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+        margin-top: 3em;
+        margin-bottom: 3em;
+    }
+
+    #artworks-table td, #artworks-table th {
+        border: 1px solid #ddd;
+        padding: 8px;
+    }
+
+    #artworks-table tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    #artworks-table tr:hover {
+        background-color: #ddd;
+    }
+
+    #artworks-table th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #4CAF50;
+        color: white;
+    }
+</style>
