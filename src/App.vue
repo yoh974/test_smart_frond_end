169a2c09393357edd1ec65e61f6486e796404377
lionel <sornom.lionel@gmail.com>
2020-07-12 11:29:40 +0200

    pagination control

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,27 @@
     <div id="app">
 
         <div>
+            <div class="custom-select" style="width:200px;">
+                <select v-on:change="getPageResult(active_page,$event.target.value)">
+                    <option value="10">10</option>
+                    <option value="20">20</option>
+                    <option selected="selected" value="30">30</option>
+                    <option value="50">50</option>
+                    <option value="100">100</option>
+                    <option value="200">200</option>
+                </select>
+            </div>
             <div>Nombre total d'oeuvres : {{nb_total_items}}</div>
             <Listing
                     :artworks="artworks"
             />
             <ul class="pagination">
                 <li class="page-item"><span class="page-link" >Previous</span></li>
-                <li class="page-item"><span class="page-link" >1</span></li>
-                <li class="page-item"><span class="page-link" v-on:click="getPageResult(2)">2</span></li>
-                <li class="page-item"><span class="page-link">3</span></li>
+                <li class="page-item"><label> Page
+                    <input id="page_number" type="number" class="page-link" v-on:keydown="keyDownControl" :value="active_page">
+                    / {{nb_total_pages}}
+                </label>
+                </li>
                 <li class="page-item"><span class="page-link">Next</span></li>
             </ul>
 
@@ -22,8 +34,13 @@
     import Listing from './components/Listing.vue'
 
     import axios from "axios"
+    let axiosInstance = axios.create({
+        baseURL: 'http://localhost:8000/api',
+        /* other custom settings */
+    });
 
-
+    let defaultPage = 1
+    let defaultItemsPerPage = 30
     export default {
 
         name: 'App',
@@ -33,33 +50,45 @@
         data() {
             return {
                 artworks: null,
-                nb_total_items: null
+                nb_total_items: null,
+                active_page: null,
+                nb_total_pages: null
             }
         },
         methods: {
-            getPageResult: function (page) {
-                axios({
-                    method: 'get',
-                    url: `http://localhost:8000/api/libraries?page=${page}`,
-                    responseType: 'application/json'
-                })
+            getPageResult: function (page,itemsPerPage=30) {
+                axiosInstance.get(`/libraries?page=${page}&itemsPerPage=${itemsPerPage}`)
                     .then(response => {
                         this.artworks = response["data"]["hydra:member"]
-
+                        this.nb_total_items = response["data"]["hydra:totalItems"]
+                        this.nb_total_pages = response["data"]["hydra:view"]["hydra:last"]
+                        this.nb_total_pages = Number(this.nb_total_pages.substr(this.nb_total_pages.indexOf("page=")+5,this.nb_total_pages.length))
                     })
+                this.active_page = page
+            },
+            keyDownControl : function (event) {
+                if (event.key === "Enter")
+                {
+                    //control the value of input
+                    var page = event.target.value
+                    if (page > this.nb_total_pages)
+                    {
+                        page = this.nb_total_pages
+
+                    }
+                    if(page < 1)
+                    {
+                        page = 1
+                    }
+
+                    this.getPageResult(page)
+                }
             }
         },
         mounted() {
             if (this.artworks === null) {
-                axios({
-                    method: 'get',
-                    url: 'http://localhost:8000/api/libraries',
-                    responseType: 'application/json',
-                })
-                    .then(response => {
-                        this.artworks = response["data"]["hydra:member"]
-                        this.nb_total_items = response["data"]["hydra:totalItems"]
-                    })
+                this.getPageResult(defaultPage,defaultItemsPerPage)
+
             }
 
         }
@@ -82,6 +111,9 @@
     }
 
     ul.pagination li {
+        display: inline;
+    }
+    ul.pagination li input{
         display: inline;
     }
 
